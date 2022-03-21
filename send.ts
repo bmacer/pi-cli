3ceca1203820b73fef7d796097ccad4d8e3e701f
bmacer <bmacer@cisco.com>
2022-03-20 23:14:03 -0400

    allow for URL input and early exit

--- send.ts
+++ send.ts
@@ -2,11 +2,21 @@ import { getSendRemark, sendRemark } from './util';
 
 const run = async () => {
     const prompt = require('prompt-sync')();
-    const item = prompt('Item: ');
-    const recipient = prompt("Recipient: ");
+    const item = prompt('Item: ').replace("https://singular.app/collectibles/", "");
+    if (item == "") {
+        console.log("Quitting...")
+        process.exit(0);
+    }
+    console.log(item);
+    const recipient = prompt("Recipient: ").replace("https://singular.app/collectibles/", "");
+    if (recipient == "") {
+        console.log("Quitting...")
+        process.exit(0);
+    }
+    console.log(recipient);
     let remark = getSendRemark(item, recipient);
     console.log(remark);
-    const cont = prompt('Enter to continue, CTRL-C to quit.');
+    const cont = prompt('Enter to continue, CTRL-C to quit: ');
     if (cont != "") {
         console.log("Quitting...");
         process.exit(0);
